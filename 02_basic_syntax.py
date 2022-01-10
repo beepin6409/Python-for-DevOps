e1f521aeae3ff513343043096565969b4831b676
Bipin Ghimire <bipin.bpn8888@gmail.com>
2022-01-10 11:52:12 +0545

    02_basic_syntax.py added

---
+++ 02_basic_syntax.py
@@ -0,0 +1,10 @@
+#string concatination
+
+a= int(input("Enter the days:"))
+print("There are " + str(60*60*24*a) + " seconds in " + str(a) + " days")
+
+you = int(input("Enter your age:"))
+father = int(input("Enter your fathers age :"))
+
+print("The sum of your and your fathers age is " + str(you + father)) 
+
