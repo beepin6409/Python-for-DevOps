4cffade0eed2730c2a808dc6db5d41cfa7d941ee
Bipin Ghimire <bipin.bpn8888@gmail.com>
2022-01-10 11:49:36 +0545

    basic syntax added

---
+++ 01_basic_syntax.py
@@ -0,0 +1,23 @@
+print("Hello World")
+var = 23
+
+   
+print(var)
+
+print(1)
+
+a=2
+
+print(a)
+
+
+print("this is a string")  #string datatype
+print(5)  # integers datatype
+print(2.66)  # float datatype 
+print(True)  # boolean datatype 
+
+
+print(20*21+60-23)  #Arithmetic Operations
+
+
+
