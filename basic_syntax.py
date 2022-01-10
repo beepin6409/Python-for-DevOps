52f2cb90908c34cfcb64cfb7cdf5f1042677c533
Bipin Ghimire <bipin.bpn8888@gmail.com>
2022-01-10 08:46:31 +0545

    basic syntax added

--- basic_syntax.py
+++ basic_syntax.py
@@ -5,3 +5,20 @@ print(1)
 a=2
 
 print(a)
+if (a>20):
+    print(f"{a} is greater then 20")
+
+else:
+    print(f"{a} is smaller then 20")
+
+
+print("this is a string")  #string datatype
+print(5)  # integers datatype
+print(2.66)  # float datatype 
+print(True)  # boolean datatype 
+
+
+print(20*21+60-23)  #Arithmetic Operations
+
+
+
