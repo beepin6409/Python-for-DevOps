5f3a6c087db48b651ae7f30060bbb3b18297f456
Bipin Ghimire <bipin.bpn8888@gmail.com>
2022-01-15 20:27:17 +0545

    dictionary added

---
+++ 06_dictionary.py
@@ -0,0 +1,21 @@
+
+bipindict={
+    "Bhatuwa":"Chutiya",
+    "Chake":"Damno",
+    "lists":[32,445,32,45]
+}
+print(bipindict)
+print(bipindict.keys())
+print(bipindict.values())
+updateddict={"RamSaili":"Chakthulo"}
+bipindict.update(updateddict)
+print(bipindict)
+a={"ChakiSAili":"Ramdi",
+"ChakiSAili2":"Ramdi",
+"ChakiSAili3":"Ramdi",
+"ChakiSAili4":"Ramdi"
+}
+bipindict.update(a) 
+
+print(bipindict)
+
